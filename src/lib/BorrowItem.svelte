3e59e2e8b9bcee290182a262ed65814210f1c2db
Ondra Janoška <o.janoska98@gmail.com>
2023-05-31 17:12:04 +0200

    kot

---
+++ src/lib/BorrowItem.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    export let image = "/mia.jpeg";
+    export let imageAltText = "cute black cat";
+    export let name = "name";
+    export let place = "place";
+    export let description = "description";
+</script>
+
+<div class="item">
+    <img src={image} alt={imageAltText}>
+    <div class="name"> {name} </div>
+    <div class="place"> {place} </div>
+    <div class="descr"> {description} </div>
+</div>
+
+<style>
+    .item {
+        display: flex;
+        min-height: 150px;
+        border: 2px solid black;
+        border-radius: 10px;
+    }
+
+    img {
+        max-width: 20vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .name {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .place {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .descr {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
--- src/routes/borrow/+page.svelte
+++ src/routes/borrow/+page.svelte
@@ -1,1 +1,17 @@
-I want to borrow stuff!!! (AMEN BROTHER!!)
+<script lang="ts">
+    import BorrowItem from "../../lib/BorrowItem.svelte";
+</script>
+haha nějaký vršek
+
+<br>
+
+<div class="results">
+    <BorrowItem />
+</div>
+
+<style>
+    .results {
+        display: grid;
+        grid-auto-flow: row;
+    }
+</style>
