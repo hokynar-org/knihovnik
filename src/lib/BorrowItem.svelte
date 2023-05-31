86bc3a8ed58d96ca1d7e2d8f075a9a867d29300f
Ondra Janoška <o.janoska98@gmail.com>
2023-05-31 18:41:58 +0200

    iwi

--- src/lib/BorrowItem.svelte
+++ src/lib/BorrowItem.svelte
@@ -2,14 +2,15 @@
     import Fa from "svelte-fa";
     import { faLocationDot, faUser } from "@fortawesome/free-solid-svg-icons";
 
-    export let image : string = "/mia.jpeg";
-    export let imageAltText : string = "cute black cat";
-    export let what : string = "Kočka";
-    export let description : string = "Kočka je černá a je to kočka.";
-    export let where : string = "Kolodějova 123";
-    export let fromWho : string = " persn" ;
-    export let mapUrl : string = "https://mapy.cz/s/3sQ5y";
-    export let lendorUrl : string = "https://www.youtube.com/watch?v=8czrx7GJa5c";
+    export let image: string = "/mia.jpeg";
+    export let imageAltText: string = "cute black cat";
+    export let what: string = "Kočka";
+    export let description: string = "Kočka je černá a je to kočka.";
+    export let where: string = "Kolodějova 123";
+    export let fromWho: string = " persn";
+    export let mapUrl: string = "https://mapy.cz/s/3sQ5y";
+    export let lendorUrl: string =
+        "https://www.youtube.com/watch?v=8czrx7GJa5c";
 </script>
 
 <article>
@@ -17,22 +18,32 @@
         <img src={image} alt={imageAltText} />
     </div>
     <div class="text">
-        <div class="name">
-            {what}
-            </div>
+        <div class="item">
+            <h4>
+                {what}
+            </h4>
             <div class="descr">{description}</div>
-            <div class="place" data-tooltip="hai"> <a href={mapUrl}> <Fa icon={faLocationDot} /> {where} </a> </div>
-            <div class="lendor"> <a href={lendorUrl}> <Fa icon={faUser} />{fromWho} </a> </div>
+        </div>
+        <div class="contact">
+            <div class="place" data-tooltip="hai">
+                <a href={mapUrl}> <Fa icon={faLocationDot} /> {where} </a>
+            </div>
+            <div class="lendor">
+                <a href={lendorUrl}> <Fa icon={faUser} />{fromWho} </a>
+            </div>
+        </div>
     </div>
 </article>
 
 <style>
     article {
         display: flex;
+        margin: 20px;
     }
     .text {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        justify-content: space-between;
         width: 100%;
     }
     img {
@@ -44,21 +55,16 @@
         margin-left: 10px;
     }
 
-    .name {
-        display: flex;
-        font-size: large;
-    }
-
     .place {
-        font-size: small;
+        font-size: medium;
         width: fit-content;
     }
     .descr {
-        font-size: small;
+        font-size: medium;
         width: fit-content;
     }
     .lendor {
-        font-size: small;
+        font-size: medium;
         width: fit-content;
     }
 </style>
