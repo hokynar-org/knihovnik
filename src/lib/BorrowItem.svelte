3b667d58515b2c51431e43d387367a46f81ec6e8
Ondra Janoška <o.janoska98@gmail.com>
2023-05-31 18:25:17 +0200

    uqu

--- src/lib/BorrowItem.svelte
+++ src/lib/BorrowItem.svelte
@@ -1,48 +1,64 @@
 <script lang="ts">
-    export let image = "/mia.jpeg";
-    export let imageAltText = "cute black cat";
-    export let name = "name";
-    export let place = "place";
-    export let description = "description";
+    import Fa from "svelte-fa";
+    import { faLocationDot, faUser } from "@fortawesome/free-solid-svg-icons";
+
+    export let image : string = "/mia.jpeg";
+    export let imageAltText : string = "cute black cat";
+    export let what : string = "Kočka";
+    export let description : string = "Kočka je černá a je to kočka.";
+    export let where : string = "Kolodějova 123";
+    export let fromWho : string = " persn" ;
+    export let mapUrl : string = "https://mapy.cz/s/3sQ5y";
+    export let lendorUrl : string = "https://www.youtube.com/watch?v=8czrx7GJa5c";
 </script>
 
-<div class="item">
-    <img src={image} alt={imageAltText}>
-    <div class="name"> {name} </div>
-    <div class="place"> {place} </div>
-    <div class="descr"> {description} </div>
-</div>
+<article>
+    <div data-tooltip={imageAltText} data-placement="top">
+        <img src={image} alt={imageAltText} />
+    </div>
+    <div class="text">
+        <div class="name">
+            {what}
+            </div>
+            <div class="descr">{description}</div>
+            <div class="place" data-tooltip="hai"> <a href={mapUrl}> <Fa icon={faLocationDot} /> {where} </a> </div>
+            <div class="lendor"> <a href={lendorUrl}> <Fa icon={faUser} />{fromWho} </a> </div>
+    </div>
+</article>
 
 <style>
-    .item {
+    article {
         display: flex;
-        min-height: 150px;
-        border: 2px solid black;
-        border-radius: 10px;
     }
-
+    .text {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
     img {
         max-width: 20vw;
         display: flex;
-        justify-content: center;
-        align-items: center;
+    }
+
+    .text {
+        margin-left: 10px;
     }
 
     .name {
         display: flex;
-        justify-content: center;
-        align-items: center;
+        font-size: large;
     }
 
     .place {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        font-size: small;
+        width: fit-content;
     }
-
     .descr {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        font-size: small;
+        width: fit-content;
+    }
+    .lendor {
+        font-size: small;
+        width: fit-content;
     }
 </style>
