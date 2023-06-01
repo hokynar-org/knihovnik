0a11ee7963dfd2250532e47ae95bd9a4e1167b3f
Michal Grňo <m93a.cz@gmail.com>
2023-06-01 15:57:02 +0200

    jůzrs lůzrs

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,15 +1,9 @@
 import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
- 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
-});
 
-// export const users = pgTable('users', {
-  //   id: serial('id').primaryKey(),
-  //   full_name: text('full_name'),
-  //   user_name: text('user_name'),
-  //   email: varchar('email', { length: 256 }),
-  //   pronouns: text('pronouns'),
-  // });
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  full_name: text("full_name"),
+  user_name: text("user_name"),
+  email: varchar("email", { length: 256 }),
+  pronouns: text("pronouns"),
+});
