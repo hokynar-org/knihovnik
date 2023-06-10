558103e26bd28020a3c7998cf1305e107af7575e
Ondra Janoška <o.janoska98@gmail.com>
2023-06-10 18:03:29 +0200

    idk some shit

--- src/lib/BorrowableItem.svelte
+++ src/lib/BorrowableItem.svelte
@@ -12,19 +12,20 @@
     export let mapUrl: string = "https://mapy.cz/s/3sQ5y";
     export let lendorUrl: string =
         "https://www.youtube.com/watch?v=8czrx7GJa5c";
-    export let offer:{item:PublicItemSafe,user:PublicUserSafe};
+    export let offer: { item: PublicItemSafe; user: PublicUserSafe };
     export let user_id: number;
 </script>
 
 <head>
-    <link rel="stylesheet" href="src/lib/style.scss">
+    <link rel="stylesheet" href="src/lib/style.scss" />
 </head>
+
 <article>
     <div data-tooltip={imageAltText} data-placement="top">
         <img src={image} alt={imageAltText} />
     </div>
     <div class="text">
-        <div class="item">
+        <div class="nameAndDesc">
             <h4>
                 {offer.item.name}
             </h4>
@@ -35,60 +36,81 @@
                 <a href={mapUrl}> <Fa icon={faLocationDot} /> {where} </a>
             </div>
             <div class="lendor">
-                <a href={lendorUrl}> <Fa icon={faUser} />{offer.user.user_name}</a>
+                <a href={lendorUrl}>
+                    <Fa icon={faUser} />{offer.user.user_name}</a
+                >
             </div>
             {#if user_id != offer.user.id}
-            <div class="borrow">
-                <form method="POST" action="borrow?/new_borrow_ask&id={offer.item.id}">
-                    <button>Borrow</button>
-                </form>
-            </div>
+                <div class="borrow">
+                    <form
+                        method="POST"
+                        action="borrow?/new_borrow_ask&id={offer.item.id}"
+                    >
+                        <button>Borrow</button>
+                    </form>
+                </div>
             {/if}
         </div>
     </div>
 </article>
 
-<style>
+<style lang="scss">
+    :root {
+        --image-height: 150px;
+        --image-width: 250px;
+        --padding: 20px;
+    }
     article {
         display: flex;
         margin: 20px;
+        background-color: var(--secondaryColor);
+        border-radius: 10px;
+        padding: var(--padding);
+        max-height: calc(var(--image-height) + 2 * var(--padding));
+    }
+    img {
+        max-width: var(--image-width);
+        max-height: var(--image-height);
+        display: flex;
     }
     .text {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         width: 100%;
-    }
-    img {
-        max-width: 20vw;
-        display: flex;
-    }
-
-    .text {
         margin-left: 10px;
+        overflow: hidden;
+    }
+    .nameAndDesc {
+        flex: 4;
     }
 
-    .place {
-        font-size: medium;
-        width: fit-content;
+    .contact {
+        display: flex;
+        flex-direction: column;
+        align-items: end;
+        flex: 1;
     }
+
     .descr {
+        display: -webkit-box;
+        -webkit-line-clamp: 4; /* Adjust the number of lines you want */
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    @mixin placeAndLendor {
         font-size: medium;
-        width: fit-content;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 1; /* Adjust the number of lines you want */
+        -webkit-box-orient: vertical;
+    }
+    .place {
+        @include placeAndLendor;
     }
     .lendor {
-        font-size: medium;
-        width: fit-content;
-    }
-    .item {
-        width: 75%;
-    }
-    .borrow{
-        margin:0;
-        padding: 0rem;
-    }
-    .borrow button{
-        margin:0;
-        padding: 0rem;
+        @include placeAndLendor;
     }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,68 +11,100 @@
 <head>
   <link rel="stylesheet" href="src/lib/style.scss" />
 </head>
-<nav>
-  <ul>
-    <li><a href="/"><h2>Knihovnik</h2></a></li>
-  </ul>
-  {#if user}
-    <ul>
-      <li>{user.user_name}, {user.role}, {user.email}</li>
-    </ul>
-  {/if}
-</nav>
-<nav>
-  <ul>
+
+<div class="scaffold">
+  <nav>
+    <a href="/"><h2>Knihovnik</h2></a>
+    {#if user}
+      <div class="userInfo">
+        {user.user_name}
+      </div>
+      {#if user.role == "ADMIN"}
+        <a href="/admin">
+          <h3>Admin</h3>
+        </a>
+      {/if}
+    {/if}
     {#if !user}
-      <li>
-        <a href="/register">
-          <h3>Register</h3>
-        </a>
-      </li>
-      <li>
-        <a href="/login">
-          <h3>Login</h3>
-        </a>
-      </li>
+      <a href="/register">
+        <h3>Register</h3>
+      </a>
+      <a href="/login">
+        <h3>Login</h3>
+      </a>
     {/if}
     {#if user}
-      <li>
-        <a href="/borrow">
-          <h3>Borrow</h3>
-        </a>
-      </li>
-      <li>
-        <a href="/offer">
-          <h3>Offer</h3>
-        </a>
-      </li>
-      <li>
-        <a href="/community">
-          <h3>Community</h3>
-        </a>
-      </li>
+      <a href="/borrow">
+        <h3>Borrow</h3>
+      </a>
+      <a href="/offer">
+        <h3>Offer</h3>
+      </a>
+      <a href="/community">
+        <h3>Community</h3>
+      </a>
     {/if}
-  </ul>
-  {#if user}
-    <ul>
-      <li>
-        <a href="/user">
-          <h3>User</h3>
-        </a>
-      </li>
-      <li>
-        <a href="/logout">
-          <h3>Logout</h3>
-        </a>
-      </li>
+    {#if user}
+      <a href="/user">
+        <h3>User</h3>
+      </a>
+      <a href="/logout">
+        <h3>Logout</h3>
+      </a>
       {#if user.role == "ADMIN"}
-        <li>
-          <a href="/admin">
-            <h3>Admin</h3>
-          </a>
-        </li>
+        <a href="/admin">
+          <h3>Admin</h3>
+        </a>
       {/if}
-    </ul>
-  {/if}
-</nav>
-<slot />
+    {/if}
+  </nav>
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style lang="scss">
+  .scaffold {
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: "nav main";
+  }
+  nav {
+    grid-area: nav;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--primaryColor);
+    padding: 20px;
+    a {
+      text-decoration: none;
+      color: var(--primaryColor);
+      :hover {
+        background-color: var(--secondaryColor);
+      }
+    }
+    @mixin marginsForNavs{
+      margin: 2px;
+      padding: 10px;
+      border-radius: 10px;
+    }
+    h2 {
+      @include marginsForNavs;
+      font-size: 2em;
+    }
+    .userInfo {
+      display: flex;
+      @include marginsForNavs;
+      background-color: var(--secondaryColor);
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    h3 {
+      @include marginsForNavs;
+    }
+  }
+  main {
+    padding: 20px;
+    grid-area: main;
+  }
+</style>
