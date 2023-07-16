7df8857a5094441d2f69b1e842c536f958eabb7d
Richard Ivánek <richard.ivanek@gmail.com>
2023-07-16 07:43:29 +0100

    Nastavení mailu se parsuje později (kvůli Vercelu)

--- src/lib/server/mail.ts
+++ src/lib/server/mail.ts
@@ -8,20 +8,18 @@ import { MAIL_AUTH } from "$env/static/private";
 //   content: URL;
 // }
 
-const SMTP_OPTIONS = z
-  .object({
-    from: z.string().email(),
-    host: z.string(),
-    port: z.number().int().positive().optional(),
-    secure: z.boolean().optional(),
-    ignoreTLS: z.boolean().optional(),
-    requireTLS: z.boolean().optional(),
-    auth: z.object({
-      user: z.string(),
-      pass: z.string(),
-    }),
-  })
-  .parse(JSON.parse(MAIL_AUTH));
+const smtp_schema = z.object({
+  from: z.string().email(),
+  host: z.string(),
+  port: z.number().int().positive().optional(),
+  secure: z.boolean().optional(),
+  ignoreTLS: z.boolean().optional(),
+  requireTLS: z.boolean().optional(),
+  auth: z.object({
+    user: z.string(),
+    pass: z.string(),
+  }),
+});
 
 const toCid = (id: string) => `${id}@knihovnik.vercel.com`;
 
@@ -30,14 +28,16 @@ export async function sendRegistrationEmail(
   address: string,
   url: string
 ) {
+  const options = smtp_schema.parse(JSON.parse(MAIL_AUTH));
+
   const text = "Potvrďte svojí registraci na adrese: " + url;
   const html = `Potvrďte svojí registraci na adrese:<br><a href="${url}">${url}</a>`;
 
-  const transporter = createTransport({ ...SMTP_OPTIONS });
+  const transporter = createTransport({ ...options });
   await transporter.sendMail({
     from: {
       name: "Knihovník Bot",
-      address: SMTP_OPTIONS.from,
+      address: options.from,
     },
     to: address,
     subject: "Registrace do Knihovníka",
