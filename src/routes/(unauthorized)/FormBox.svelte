d70991af5672e83d1196c6aa23b54389907127f2
Richard Ivánek <richard.ivanek@gmail.com>
2023-08-05 13:24:06 +0100

    feat: add calculated tailwind colors

--- src/routes/(unauthorized)/FormBox.svelte
+++ src/routes/(unauthorized)/FormBox.svelte
@@ -2,7 +2,9 @@
   export let subtitle: string | undefined = undefined;
 </script>
 
-<div class="md:min-w-[420px] md:mt-48 p-4 border-0 border-muted rounded-xl">
+<div
+  class="md:min-w-[420px] md:mt-48 p-4 md:bg-surface border-0 border-muted rounded-xl"
+>
   <h1 class="text-center text-4xl">Knihovník</h1>
   {#if subtitle}
     <h2 class="text-center text-lg text-muted">{subtitle}</h2>
