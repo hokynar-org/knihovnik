398f15fcd9c42d4630fc1a8d3beab6e8a6131b74
Richard Ivánek <richard.ivanek@gmail.com>
2023-08-06 14:00:28 +0100

    feat: add schema to drizzle client constructor for relational queries functionality

--- src/lib/server/db/drizzle.ts
+++ src/lib/server/db/drizzle.ts
@@ -1,17 +1,21 @@
 import Pg from 'pg';
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { DATABASE_URL } from '$env/static/private';
+import * as schema from './schema';
 
 declare let global: typeof globalThis & {
-  db: NodePgDatabase | undefined;
+  db: NodePgDatabase<typeof schema> | undefined;
 };
 
-const db: NodePgDatabase =
+const db: NodePgDatabase<typeof schema> =
   global.db ||
   drizzle(
     new Pg.Pool({
       connectionString: DATABASE_URL + '?sslmode=require',
     }),
+    {
+      schema,
+    },
   );
 
 // Save the instance outside of the scope of this script for reuse
