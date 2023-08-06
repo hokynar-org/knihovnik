34bd965d2ed4ed73a560a5cd3f3871f6c0a0883e
Richard Ivánek <richard.ivanek@gmail.com>
2023-08-06 23:39:15 +0100

    fix: use AppRail in (unauthorized)

--- src/routes/(unauthorized)/+layout.svelte
+++ src/routes/(unauthorized)/+layout.svelte
@@ -1,7 +1,11 @@
-<div class="min-h-full flex flex-col items-center">
+<script>
+  import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell slotPageContent="flex flex-col items-center">
   <slot />
 
   <footer class="mt-auto text-center text-surface-500-400-token p-2">
     Knihovník by Hokynář <span class="inline-block mx-2">|</span> 2023
   </footer>
-</div>
+</AppShell>
