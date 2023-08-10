775dba2a7c9ce172f581c6869b8a6c8248fef407
Ondřej Knopp <knopp@karel.troja.mff.cuni.cz>
2023-08-10 03:23:48 +0200

    message notification

--- src/routes/(authorized)/api/borrow_request/[borrow_request_id]/message/+server.ts
+++ src/routes/(authorized)/api/borrow_request/[borrow_request_id]/message/+server.ts
@@ -1,9 +1,9 @@
 import { error, fail, json, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { db } from '$lib/server/db/drizzle';
-import {borrow_requests, request_actions} from '$lib/server/db/schema'
+import {borrow_requests, items, notifications, request_actions} from '$lib/server/db/schema'
 import { eq } from 'drizzle-orm';
-import type { BorrowRequest } from '$lib/types';
+import type { Notification, RequestAction } from '$lib/types';
 
 export const POST = (async ({ request, params, locals, url, route }) => {
   if (!locals.user) {
@@ -16,22 +16,43 @@ export const POST = (async ({ request, params, locals, url, route }) => {
   const message = body.message;
   const user_id = locals.user.id;
   const borrow_request_id = params.borrow_request_id as string;
-  const found_borrow_requests:Array<BorrowRequest> = await db.select().from(borrow_requests).where(eq(borrow_requests.id, Number(borrow_request_id)));
+  const found_borrow_requests = await db.select({
+    item: {
+      name: items.name,
+      description: items.description,
+      id: items.id,
+      owner_id: items.owner_id,
+    },
+    borrow_request: {
+      status: borrow_requests.status,
+      id: borrow_requests.id,
+      borrower_id: borrow_requests.borrower_id,
+      lender_id: borrow_requests.lender_id,
+      item_id: borrow_requests.item_id,
+      timestamp: borrow_requests.timestamp,
+    },}).from(borrow_requests).where(eq(borrow_requests.id, Number(borrow_request_id))).innerJoin(items,eq(items.id,borrow_requests.item_id));
   if(found_borrow_requests.length==0) {
     throw error(400);
   }
-  const borrow_request=found_borrow_requests[0];
+  const borrow_request=found_borrow_requests[0].borrow_request;
+  const item = found_borrow_requests[0].item;
   if(borrow_request.lender_id!=user_id && borrow_request.borrower_id!=user_id){
     throw error(401);
   }
   try {
-    const new_requests_actions = await db.insert(request_actions).values({
+    const new_requests_actions:Promise<RequestAction[]> = db.insert(request_actions).values({
       borrow_request_id:Number(borrow_request_id),
       user_id:user_id,
       type: 'MESSAGE',
       message: message,
       }).returning();
-    return json(new_requests_actions[0]);
+    const message_notification:Promise<Notification[]> = db.insert(notifications).values({
+        user_id: user_id==borrow_request.lender_id?borrow_request.borrower_id:borrow_request.lender_id,
+        text: "User " + locals.user.user_name + " messaged you concerning " + item.name,
+        url: '/borrow_request/'+String(borrow_request.id),
+      }).returning();
+    const results = await Promise.all([new_requests_actions,message_notification])
+    return json(results[0][0]);
   } catch (err) {
     throw error(500);
   }
--- src/routes/(tests)/pusher/+page.server.ts
+++ src/routes/(tests)/pusher/+page.server.ts
@@ -1,5 +1,5 @@
 import { db } from '$lib/server/db/drizzle';
-import { borrow_requests, request_actions } from '$lib/server/db/schema';
+import { borrow_requests, notifications, request_actions } from '$lib/server/db/schema';
 import { pusher } from '$lib/server/pusher'
 import { eq } from 'drizzle-orm';
 import type { PageServerLoad } from './$types';
@@ -8,7 +8,8 @@ export const load: PageServerLoad = async () => {
     const log = await pusher.trigger("channel-1", "test_event", { message: "hello world" },);
     const delete_borrow_requests:Promise<any> = db.delete(borrow_requests);
     const delete_request_actions:Promise<any> = db.delete(request_actions);
-    const results:any = await Promise.all([delete_borrow_requests,delete_request_actions]);
+    const delete_notifications  :Promise<any> = db.delete(notifications);
+    const results:any = await Promise.all([delete_borrow_requests,delete_request_actions,delete_notifications]);
     console.log(log);
     return {}
 };
