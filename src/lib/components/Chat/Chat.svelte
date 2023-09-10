0101547ad4af156f9f1e274be3144d544ab7fd16
Jindra <jindra@immerda.ch>
2023-09-10 22:01:51 +0200

    Lock chat if PENDING or CONFIRMED

--- src/lib/components/Chat/Chat.svelte
+++ src/lib/components/Chat/Chat.svelte
@@ -55,11 +55,17 @@
     {/each}
   </table>
 </div>
-<div class="flex my-2 w-full">
-  <input class="input" type="text" bind:value={$composedMessage} />
-  {#if isCommunityMessages(messages) && isCommunityNotNull(community)}
+
+{#if isCommunityMessages(messages) && isCommunityNotNull(community)}
+  <div class="flex my-2 w-full">
+    <input class="input" type="text" bind:value={$composedMessage} />
     <ChatSendCommunity {community} {messages} />
-  {:else if isRequestMessages(messages) && isBorrowRequestNotNull(borrow_request)}
-    <ChatSendRequest {borrow_request} />
+  </div>
+{:else if isRequestMessages(messages) && isBorrowRequestNotNull(borrow_request)}
+  {#if borrow_request.status != ('CONFIRMED' || 'PENDING')}
+    <div class="flex my-2 w-full">
+      <input class="input" type="text" bind:value={$composedMessage} />
+      <ChatSendRequest {borrow_request} />
+    </div>
   {/if}
-</div>
+{/if}
