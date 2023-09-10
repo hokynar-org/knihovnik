15dab2873c9785272b977c2e5eb62d4b52d0f60e
Ondřej Knopp <knopp@karel.troja.mff.cuni.cz>
2023-09-10 22:10:35 +0200

    oops 2

--- src/lib/components/Chat/Chat.svelte
+++ src/lib/components/Chat/Chat.svelte
@@ -7,13 +7,16 @@
     PublicUserSafe,
     RequestActionMessage,
     Community,
+    BorrowRequest,
   } from '$lib/types';
   import { composedMessage } from '$lib/components/Chat/stores';
+  import ChatSendRequest from './ChatSendRequest.svelte';
 
   export let messages: CommunityMessage[] | RequestActionMessage[];
   export let user: PublicUserSafe; //To determine who writes "your" messages
   //export let isadmin: Boolean; //To be used to determine if you can delete messages
   export let community = null as null | Community; //Which community does this chat belong to?
+  export let borrow_request = null as null | BorrowRequest; //Which borrow request does this chat belong to?
 
   function isCommunityMessages(
     messages: CommunityMessage[] | RequestActionMessage[],
@@ -21,10 +24,21 @@
     //Only CommunityMessage contains the property community_id
     return messages.every((item) => 'community_id' in item);
   }
-  function isCommunityNull(
+  function isCommunityNotNull(
     community: null | Community,
   ): community is Community {
-    return community === null;
+    return !(community === null);
+  }
+
+  function isRequestMessages(
+    messages: CommunityMessage[] | RequestActionMessage[],
+  ): messages is RequestActionMessage[] {
+    return messages.every((item) => 'borrow_request_id' in item);
+  }
+  function isBorrowRequestNotNull(
+    borrow_request: null | BorrowRequest,
+  ): borrow_request is BorrowRequest {
+    return !(borrow_request === null);
   }
 
   let element: HTMLDivElement;
@@ -41,9 +55,17 @@
     {/each}
   </table>
 </div>
-<div class="flex my-2 w-full">
-  <input class="input" type="text" bind:value={$composedMessage} />
-  {#if isCommunityMessages(messages) && community}
+
+{#if isCommunityMessages(messages) && isCommunityNotNull(community)}
+  <div class="flex my-2 w-full">
+    <input class="input" type="text" bind:value={$composedMessage} />
     <ChatSendCommunity {community} {messages} />
+  </div>
+{:else if isRequestMessages(messages) && isBorrowRequestNotNull(borrow_request)}
+  {#if borrow_request.status != ('CONFIRMED' || 'PENDING')}
+    <div class="flex my-2 w-full">
+      <input class="input" type="text" bind:value={$composedMessage} />
+      <ChatSendRequest {borrow_request} />
+    </div>
   {/if}
-</div>
+{/if}
