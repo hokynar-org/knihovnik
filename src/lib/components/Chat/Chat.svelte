0004b1a8075b10e676d20040a9ca17eaec59cd78
Jindra <jindra@immerda.ch>
2023-10-05 21:37:55 +0200

    Can't send message if borrow request denied - hopefully working

--- src/lib/components/Chat/Chat.svelte
+++ src/lib/components/Chat/Chat.svelte
@@ -62,7 +62,7 @@
     <ChatSendCommunity {community} {messages} />
   </div>
 {:else if isRequestMessages(messages) && isBorrowRequestNotNull(borrow_request)}
-  {#if borrow_request.status !== 'CONFIRMED' && borrow_request.status !== 'PENDING' && borrow_request.status !== 'ABORTED'}
+  {#if borrow_request.status !== 'CONFIRMED' && borrow_request.status !== 'PENDING' && borrow_request.status !== 'ABORTED' && borrow_request.status !== 'DENIED'}
     <div class="flex my-2 w-full">
       <input class="input" type="text" bind:value={$composedMessage} />
       <ChatSendRequest {borrow_request} />
