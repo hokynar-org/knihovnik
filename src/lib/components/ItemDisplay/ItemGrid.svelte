f62c2385ea8e62f64fcd13a99b9ceb1a49799451
Jindra <jindra@immerda.ch>
2023-10-05 20:59:20 +0200

    Better Item Grid

---
+++ src/lib/components/ItemDisplay/ItemGrid.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  let fontSize = 16; //1 rem = this much px
+  let width = 1352;
+  let containerWidth = 1352;
+
+  export let cardWidth = 20;
+  export let cardGap = 1.5;
+  export let cls = '';
+
+  $: console.log(containerWidth);
+  //Choose a maximum amount of rows, but have the width be fit to the rows.
+  //A horrible solution that doesn't update often
+  //desired width:
+  //n * 20rem + (n-1)1.5rem = n*(21.5rem) - 1.5rem
+  //20rem = card width
+  //1.5rem = gap between cards
+  //Equation:
+  //n*(21.5rem) - 1.5rem = maxwidth -> and then n floor
+  $: maxwidth = containerWidth * 0.95;
+  $: n = Math.floor(
+    (maxwidth + 1.95 * fontSize) / ((cardWidth + cardGap) * fontSize),
+  );
+  $: width = (n * (cardWidth + cardGap) - cardGap) * fontSize;
+  //$: console.log('n: ' + String(n));
+  //$: console.log('width: ' + String(width));
+  //$: console.log('font-size: ' + String(fontSize));
+  onMount(() => {
+    let reference = document.getElementById('measuring');
+    if (reference) {
+      const computedStyle = window.getComputedStyle(reference);
+      fontSize = parseFloat(computedStyle.getPropertyValue('font-size'));
+    }
+  });
+</script>
+
+<div id="measuring" bind:clientWidth={containerWidth} class="w-[100%]"></div>
+<div
+  class="flex flex-wrap flex-shrink custom-width gap-6 {cls}"
+  style="--my-width:{width}"
+>
+  <slot />
+</div>
+
+<style>
+  .custom-width {
+    width: calc(var(--my-width) * 1px);
+  }
+</style>
--- src/routes/(authorized)/offer/+page.svelte
+++ src/routes/(authorized)/offer/+page.svelte
@@ -1,52 +1,17 @@
 <script lang="ts">
-  import { superForm } from 'sveltekit-superforms/client';
-  import Item from '$lib/components/ItemDisplay/Item.svelte';
   import ItemCard from '$lib/components/ItemDisplay/ItemCard.svelte';
   import { user_items } from '$lib/store';
-  import { FileDropzone } from '@skeletonlabs/skeleton';
-  import { onMount } from 'svelte';
   import DeleteItem from '$lib/components/ItemDisplay/DeleteItem.svelte';
-  import FileUploader from '$lib/components/FileUploader.svelte';
   import type { PageData } from './$types';
   import OfferItem from '$lib/OfferItem.svelte';
+  import ItemGrid from '$lib/components/ItemDisplay/ItemGrid.svelte';
   export let data: PageData;
   $: user = data.user;
 
   $: $user_items = data.user_items;
-
-  let fontSize = 16; //1 rem = this much px
-
-  let width = 1352;
-
-  let containerWidth = 1352;
-
-  $: console.log(containerWidth);
-  //Choose a maximum amount of rows, but have the width be fit to the rows.
-  //A horrible solution that doesn't update often
-  //desired width:
-  //n * 20rem + (n-1)1.5rem = n*(21.5rem) - 1.5rem
-  //Equation:
-  //n*(21.5rem) - 1.5rem = maxwidth -> and then n floor
-  $: maxwidth = containerWidth * 0.95;
-  $: n = Math.floor((maxwidth + 1.95 * fontSize) / (21.5 * fontSize));
-  $: width = (n * 21.5 - 1.5) * fontSize;
-  //$: console.log('n: ' + String(n));
-  //$: console.log('width: ' + String(width));
-  //$: console.log('font-size: ' + String(fontSize));
-  onMount(() => {
-    let reference = document.getElementById('measuring');
-    if (reference) {
-      const computedStyle = window.getComputedStyle(reference);
-      fontSize = parseFloat(computedStyle.getPropertyValue('font-size'));
-    }
-  });
 </script>
 
-<div id="measuring" bind:clientWidth={containerWidth} class="w-[100%]"></div>
-<div
-  class="flex flex-wrap flex-shrink custom-width mt-6 gap-6"
-  style="--my-width:{width}"
->
+<ItemGrid cls="mt-6">
   {#each $user_items as offer (offer.item.id)}
     <ItemCard item={offer.item} owner={user} holder={offer.holder}>
       {#if offer.holder && user.id == offer.holder.id}
@@ -61,10 +26,4 @@
       {/if}
     </ItemCard>
   {/each}
-</div>
-
-<style>
-  .custom-width {
-    width: calc(var(--my-width) * 1px);
-  }
-</style>
+</ItemGrid>
