ccb74a4e6e010e3a24d3b289c57d58e346dd4ca0
Jindra <jindra@immerda.ch>
2023-10-20 21:45:11 +0200

    ItemPageBar now uses buttons

--- src/lib/components/ItemDisplay/ItemPageBar.svelte
+++ src/lib/components/ItemDisplay/ItemPageBar.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
+  import Fa from 'svelte-fa';
   import {
     faArrowLeft,
     faArrowRight,
     faMagnifyingGlass,
   } from '@fortawesome/free-solid-svg-icons';
-  import Fa from 'svelte-fa';
   export let limit: number;
   export let length: number;
   export let offset: number;
   export let root: string;
+  export let search: string | null = null;
+
   function enterPressed(event: KeyboardEvent): void {
     if (event.key === 'Enter' || event.code === 'Enter') {
       goto(
@@ -20,7 +22,17 @@
       );
     }
   }
-  export let search: string | null = null;
+
+  let leftURL = '' as string;
+
+  $: leftURL =
+    root +
+    '?offset=' +
+    Math.max(offset - limit, 0) +
+    '&limit=' +
+    Math.max(limit, 1) +
+    (search && search!.length > 0 ? '&search=' + search : '');
+  $: console.log(leftURL);
 </script>
 
 <div class="grid grid-cols-[5fr,1fr] mt-6 gap-2 items-center w-min-fit">
@@ -42,41 +54,35 @@
     </a>
   </div>
   <div class="grid grid-cols-2">
-    {#if offset - limit >= 0}
-      <a
-        class="w-fit btn-icon h-10 variant-filled-primary rounded-r-none"
-        href="{root}?offset={Math.max(offset - limit, 0)}&limit={Math.max(
-          limit,
-          1,
-        )}{search && search.length > 0 ? '&search=' + search : ''}"
-      >
-        <Fa icon={faArrowLeft} />
-      </a>
-    {:else}
-      <button
-        disabled={true}
-        class="w-fit btn-icon h-10 variant-filled-primary rounded-r-none"
-      >
-        <Fa icon={faArrowLeft} />
-      </button>
-    {/if}
-    {#if offset + limit < length}
-      <a
-        class="w-fit btn-icon h-10 variant-filled-primary rounded-l-none"
-        href="{root}?offset={offset + limit}&limit={Math.max(
-          limit,
-          1,
-        )}{search && search.length > 0 ? '&search=' + search : ''}"
-      >
-        <Fa icon={faArrowRight} />
-      </a>
-    {:else}
-      <button
-        disabled={true}
-        class="w-fit btn-icon h-10 variant-filled-primary rounded-l-none"
-      >
-        <Fa icon={faArrowRight} />
-      </button>
-    {/if}
+    <button
+      disabled={!(offset - limit >= 0)}
+      class="w-fit btn-icon h-10 variant-filled-primary rounded-r-none"
+      on:click={() =>
+        goto(
+          root +
+            '?offset=' +
+            Math.max(offset - limit, 0) +
+            '&limit=' +
+            Math.max(limit, 1) +
+            (search && search.length > 0 ? '&search=' + search : ''),
+        )}
+    >
+      <Fa icon={faArrowLeft} />
+    </button>
+    <button
+      disabled={!(offset + limit < length)}
+      class="w-fit btn-icon h-10 variant-filled-primary rounded-l-none"
+      on:click={() =>
+        goto(
+          root +
+            '?offset=' +
+            (offset + limit) +
+            '&limit=' +
+            Math.max(limit, 1) +
+            (search && search.length > 0 ? '&search=' + search : ''),
+        )}
+    >
+      <Fa icon={faArrowRight} />
+    </button>
   </div>
 </div>
