43d2df95ac1c0b57a14b8311c236ddeb7b98b2dd
Jindra <jindra@immerda.ch>
2023-10-21 11:32:05 +0200

    QOL changes in community_id admin

--- src/routes/(authorized)/community/[community_id]/admin/+page.svelte
+++ src/routes/(authorized)/community/[community_id]/admin/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-  import type { CommunityRelation, PublicUserSafe } from '$lib/types';
+  import CommunityUsers from '$lib/components/Community/CommunityUsers.svelte';
+  import type {
+    CommunityRelation,
+    PublicUserSafe,
+    CommunityUserSafe,
+  } from '$lib/types';
   export let data;
   $: community = data.community;
   $: community_users = data.community_users;
@@ -30,6 +35,18 @@
     }
     return (await res.json()) as CommunityRelation;
   };
+  function invite_pressed(user: PublicUserSafe): void {
+    disabled = true;
+    invite(user.id)
+      .then((value) => {
+        community_users = [...community_users, { relation: value, user: user }];
+        disabled = false;
+      })
+      .catch((reason) => {
+        disabled = false;
+      });
+  }
+
   const kick = async (user_id: number) => {
     const res = await fetch(
       '/api/community/' + community.id + '/' + user_id + '/kick',
@@ -42,30 +59,21 @@
     }
     return (await res.json()) as CommunityRelation;
   };
-  const accept = async (user_id: number) => {
-    const res = await fetch(
-      '/api/community/' + community.id + '/' + user_id + '/accept',
-      {
-        method: 'POST',
-      },
-    );
-    if (!res.ok) {
-      throw new Error(String(res.status));
-    }
-    return (await res.json()) as CommunityRelation;
-  };
-  const deny = async (user_id: number) => {
-    const res = await fetch(
-      '/api/community/' + community.id + '/' + user_id + '/deny',
-      {
-        method: 'POST',
-      },
-    );
-    if (!res.ok) {
-      throw new Error(String(res.status));
-    }
-    return (await res.json()) as CommunityRelation;
-  };
+  function kick_pressed(community_user: CommunityUserSafe): void {
+    disabled = true;
+
+    kick(community_user.user.id)
+      .then((value) => {
+        community_users = community_users.filter((fvalue) => {
+          return value.user_id !== fvalue.user.id;
+        });
+        disabled = false;
+      })
+      .catch((reason) => {
+        disabled = false;
+      });
+  }
+
   const promote = async (user_id: number) => {
     const res = await fetch(
       '/api/community/' + community.id + '/' + user_id + '/promote',
@@ -78,6 +86,92 @@
     }
     return (await res.json()) as CommunityRelation;
   };
+  function promote_pressed(community_user: CommunityUserSafe): void {
+    disabled = true;
+
+    promote(community_user.user.id)
+      .then((value) => {
+        community_users = community_users.flatMap((fvalue) => {
+          if (value.user_id != fvalue.user.id) {
+            return fvalue;
+          } else {
+            return { relation: value, user: fvalue.user };
+          }
+        });
+        disabled = false;
+      })
+      .catch((reason) => {
+        disabled = false;
+      });
+  }
+
+  const accept = async (user_id: number) => {
+    const res = await fetch(
+      '/api/community/' + community.id + '/' + user_id + '/accept',
+      {
+        method: 'POST',
+      },
+    );
+    if (!res.ok) {
+      throw new Error(String(res.status));
+    }
+    return (await res.json()) as CommunityRelation;
+  };
+  function accept_pressed(community_user: CommunityUserSafe): void {
+    disabled = true;
+    accept(community_user.user.id)
+      .then((value) => {
+        community_users = community_users.flatMap((fvalue) => {
+          if (value.user_id != fvalue.user.id) {
+            return fvalue;
+          } else {
+            return { relation: value, user: fvalue.user };
+          }
+        });
+        disabled = false;
+      })
+      .catch((reason) => {
+        disabled = false;
+      });
+  }
+
+  const deny = async (user_id: number) => {
+    const res = await fetch(
+      '/api/community/' + community.id + '/' + user_id + '/deny',
+      {
+        method: 'POST',
+      },
+    );
+    if (!res.ok) {
+      throw new Error(String(res.status));
+    }
+    return (await res.json()) as CommunityRelation;
+  };
+  function deny_pressed(community_user: CommunityUserSafe): void {
+    disabled = true;
+    deny(community_user.user.id)
+      .then((value) => {
+        community_users = community_users.filter((fvalue) => {
+          return value.user_id != fvalue.user.id;
+        });
+        disabled = false;
+      })
+      .catch((reason) => {
+        disabled = false;
+      });
+  }
+
+  //Only use this if you know that the User is in the community
+  function UsertoCommunityUser(
+    community_users: CommunityUserSafe[],
+    user: PublicUserSafe,
+  ): CommunityUserSafe {
+    let found: CommunityUserSafe;
+    found = community_users.find(
+      (community_user) => community_user.user.id === user.id,
+    )!;
+    return found;
+  }
 
   //Find and invite users: enter to press
   function button_clicked_faiu() {
@@ -115,89 +209,31 @@
       <button
         {disabled}
         class="btn variant-filled-error py-1 my-2"
-        on:click={() => {
-          disabled = true;
-
-          kick(community_user.user.id)
-            .then((value) => {
-              community_users = community_users.filter((fvalue) => {
-                value.user_id != fvalue.user.id;
-              });
-              disabled = false;
-            })
-            .catch((reason) => {
-              disabled = false;
-            });
-        }}
+        on:click={() => kick_pressed(community_user)}
       >
         Kick
       </button>
       <button
         class="btn variant-filled-primary py-1 my-2"
         {disabled}
-        on:click={() => {
-          disabled = true;
-
-          promote(community_user.user.id)
-            .then((value) => {
-              community_users = community_users.flatMap((fvalue) => {
-                if (value.user_id != fvalue.user.id) {
-                  return fvalue;
-                } else {
-                  return { relation: value, user: fvalue.user };
-                }
-              });
-              disabled = false;
-            })
-            .catch((reason) => {
-              disabled = false;
-            });
-        }}
+        on:click={() => promote_pressed(community_user)}
       >
         Promote
       </button>
     {:else if community_user.relation.role == 'REQUESTED'}
       <button
-        class="btn variant-filled-primary py-1 my-2"
-        {disabled}
-        on:click={() => {
-          disabled = true;
-          accept(community_user.user.id)
-            .then((value) => {
-              community_users = community_users.flatMap((fvalue) => {
-                if (value.user_id != fvalue.user.id) {
-                  return fvalue;
-                } else {
-                  return { relation: value, user: fvalue.user };
-                }
-              });
-              disabled = false;
-            })
-            .catch((reason) => {
-              disabled = false;
-            });
-        }}
-      >
-        Accept
-      </button>
-      <button
         {disabled}
         class="btn variant-filled-error py-1 my-2"
-        on:click={() => {
-          disabled = true;
-          deny(community_user.user.id)
-            .then((value) => {
-              community_users = community_users.filter((fvalue) => {
-                value.user_id != fvalue.user.id;
-              });
-              disabled = false;
-            })
-            .catch((reason) => {
-              disabled = false;
-            });
-        }}
+        on:click={() => deny_pressed(community_user)}
       >
         Deny
+      </button>
+      <button
+        class="btn variant-filled-primary py-1 my-2"
+        {disabled}
+        on:click={() => accept_pressed(community_user)}
+      >
+        Accept
       </button>
     {/if}
     <br />
@@ -231,27 +267,47 @@
         <button
           {disabled}
           class="btn variant-filled-primary py-1 my-2"
-          on:click={() => {
-            disabled = true;
-            invite(user.id)
-              .then((value) => {
-                community_users = [
-                  ...community_users,
-                  { relation: value, user: user },
-                ];
-                disabled = false;
-              })
-              .catch((reason) => {
-                disabled = false;
-              });
-          }}
+          on:click={() => invite_pressed(user)}
         >
           Invite
         </button>
       {:else}
-        ({community_users.find(
-          (community_user) => community_user.user.id === user.id,
-        )?.relation.role})
+        ({UsertoCommunityUser(community_users, user).relation.role})
+        {#if UsertoCommunityUser(community_users, user).relation.role == 'MEMBER'}
+          <button
+            {disabled}
+            class="btn variant-filled-error py-1 my-2"
+            on:click={() =>
+              kick_pressed(UsertoCommunityUser(community_users, user))}
+          >
+            Kick
+          </button>
+          <button
+            class="btn variant-filled-primary py-1 my-2"
+            {disabled}
+            on:click={() =>
+              promote_pressed(UsertoCommunityUser(community_users, user))}
+          >
+            Promote
+          </button>
+        {:else if UsertoCommunityUser(community_users, user).relation.role == 'REQUESTED'}
+          <button
+            {disabled}
+            class="btn variant-filled-error py-1 my-2"
+            on:click={() =>
+              deny_pressed(UsertoCommunityUser(community_users, user))}
+          >
+            Deny
+          </button>
+          <button
+            class="btn variant-filled-primary py-1 my-2"
+            {disabled}
+            on:click={() =>
+              accept_pressed(UsertoCommunityUser(community_users, user))}
+          >
+            Accept
+          </button>
+        {/if}
       {/if}
     {/each}
   </div>
