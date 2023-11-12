0cae03ac600e243155da653fd20c05fa052ea88d
Ondřej Knopp <knopp@karel.troja.mff.cuni.cz>
2023-11-12 03:25:51 +0100

    better paginator

--- src/lib/components/ItemDisplay/ItemPaginator.svelte
+++ src/lib/components/ItemDisplay/ItemPaginator.svelte
@@ -1,44 +1,53 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+  import {
+    faArrowLeft,
+    faArrowRight,
+    faEllipsis,
+  } from '@fortawesome/free-solid-svg-icons';
   import Fa from 'svelte-fa';
+  import PageButton from './PageButton.svelte';
   export let limit: number;
   export let offset: number;
   export let length: number;
   export let root: string;
   export let search: string | null;
   export let cls = '' as string;
+  const pagesShown = (length: number, limit: number, offset: number) => {
+    const pagesTotal = Math.max(Math.ceil(length / limit), 1);
+    const thisPage = Math.max(Math.floor(offset / limit) + 1, 1);
+    const pgs: number[] = [];
+    for (let i = 1; i <= pagesTotal; i++) {
+      if (
+        i == 1 ||
+        i == pagesTotal ||
+        i == thisPage ||
+        Math.abs(i - thisPage) <= 1
+      ) {
+        pgs.push(i);
+      }
+    }
+    return pgs;
+  };
+  let pages = pagesShown(length, limit, offset);
+  $: pages = pagesShown(length, limit, offset);
 </script>
 
-<div class="flex gap-0 {cls}">
-  <button
-    disabled={!(offset - limit >= 0)}
-    class="w-20 btn-icon h-10 variant-filled-primary rounded-r-none"
-    on:click={() =>
-      goto(
-        root +
-          '?offset=' +
-          Math.max(offset - limit, 0) +
-          '&limit=' +
-          Math.max(limit, 1) +
-          (search && search.length > 0 ? '&search=' + search : ''),
-      )}
-  >
-    <Fa icon={faArrowLeft} />
-  </button>
-  <button
-    disabled={!(offset + limit < length)}
-    class="w-20 btn-icon h-10 variant-filled-primary rounded-l-none"
-    on:click={() =>
-      goto(
-        root +
-          '?offset=' +
-          (offset + limit) +
-          '&limit=' +
-          Math.max(limit, 1) +
-          (search && search.length > 0 ? '&search=' + search : ''),
-      )}
-  >
-    <Fa icon={faArrowRight} />
-  </button>
-</div>
+{#if length > 0}
+  <div class="flex gap-0 {cls}">
+    {#each pages as page, index (page)}
+      {#if index > 0 && pages[index - 1] != page - 1}
+        <div class="w-10 h-10 flex items-center justify-center">
+          <Fa icon={faEllipsis} />
+        </div>
+      {/if}
+      <PageButton
+        {page}
+        {limit}
+        {root}
+        {search}
+        disabled={page == Math.max(Math.floor(offset / limit) + 1, 1)}
+      />
+    {/each}
+  </div>
+{/if}
---
+++ src/lib/components/ItemDisplay/PageButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let page: number;
+  export let limit: number;
+  export let root: string;
+  export let search: string | null;
+  export let disabled: boolean;
+</script>
+
+<div class="group">
+  {#if disabled}
+    <button
+      class="w-10 btn-icon h-10 variant-filled-primary rounded-none group-last:rounded-r-full group-first:rounded-l-full"
+      disabled={true}>{page}</button
+    >
+  {:else}
+    <a
+      href="{root}?offset={(page - 1) * limit}&limit={Math.max(
+        limit,
+        1,
+      )}{search && search.length > 0 ? '&search=' + search : ''}"
+    >
+      <button
+        class="w-10 btn-icon h-10 variant-filled-primary rounded-none group-last:rounded-r-full group-first:rounded-l-full"
+        >{page}</button
+      ></a
+    >
+  {/if}
+</div>
